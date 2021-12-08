2123bcae639d6210d675a2e6347c20963bc41ee5
Peter <warrenp11@gmail.com>
2021-12-08 14:38:18 -0500

    isEven algo

---
+++ algorithms.js
@@ -0,0 +1,15 @@
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// console.log(isEven(4));
+// console.log(isEven(44));
+// console.log(isEven(444));
+// console.log(isEven(41));
+// console.log(isEven(34));
+// console.log(isEven(401));
+// console.log(isEven(455));
