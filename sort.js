1c7223b1a60161546bd8fe79e4514226b17c023d
Peter <warrenp11@gmail.com>
2021-12-08 18:15:36 -0500

    quick sort without recursion returns somewhat sorted array

--- sort.js
+++ sort.js
@@ -25,4 +25,25 @@ const bubbleSort = (arr) => {
   return arr;
 };
 
-module.exports = { bubbleSort };
+const quickSort = (arr) => {
+  // use first index as the pivot point
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  // start at index 1 to ignore pivot
+  for (let i = 1; i < arr.length; i++) {
+    // push into different arrays based on value compared to pivot
+    if (arr[i] <= pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  // merge arrays and pivot together
+  return left.concat(pivot, right);
+};
+
+// export along with bubble sort
+module.exports = { bubbleSort, quickSort };
