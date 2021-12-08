c8602ff2300d48e3eaf9834b26b3f76c1af51feb
Peter <warrenp11@gmail.com>
2021-12-08 14:44:40 -0500

    findIndex algo

--- algorithms.js
+++ algorithms.js
@@ -13,3 +13,21 @@ const isEven = (num) => {
 // console.log(isEven(34));
 // console.log(isEven(401));
 // console.log(isEven(455));
+
+const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+
+const findIndex = (num) => {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] === num) {
+      console.log(`${num} found at index ${i}`);
+    }
+  }
+};
+
+console.log(findIndex(12)); // found
+console.log(findIndex(45)); // undefined
+console.log(findIndex(40)); 
+console.log(findIndex(99));
+console.log(findIndex(71));
+console.log(findIndex(21));
+console.log(findIndex(87));
