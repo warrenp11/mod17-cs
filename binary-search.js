2ee6e31d1fc2b5bd77d3c170a570f92b8ff30539
Peter <warrenp11@gmail.com>
2021-12-08 16:21:16 -0500

    binary search with recursion

--- binary-search.js
+++ binary-search.js
@@ -1,5 +1,6 @@
 const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
 
+// Binary Search without Recursion
 const binarySearch = (arr, num) => {
   let left = 0;
   let right = arr.length - 1;
@@ -16,6 +17,29 @@ const binarySearch = (arr, num) => {
   }
 };
 
+// Console logs for binary search without recursion
 console.log(binarySearch(data, 38));
 console.log(binarySearch(data, 71));
 console.log(binarySearch(data, 54));
+
+// Binary Search with Recursion
+const binarySearch = (arr, num, left, right) => {
+  let middle = Math.floor((left + right) / 2);
+
+  // range overlapped, so never found number
+  if (left > right) {
+    return -1;
+  } else if (num === arr[middle]) {
+    return middle;
+  } else if (num < arr[middle]) {
+    // call again with a new right value
+    return binarySearch(arr, num, left, middle - 1);
+  } else {
+    // call again with a new left value
+    return binarySearch(arr, num, middle + 1, right);
+  }
+};
+
+// Console logs for binary search with recursion
+// set initial left and right values on first call
+console.log(binarySearch(data, 12, 0, data.length - 1));
