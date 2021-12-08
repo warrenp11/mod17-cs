f04c7a480cf7ed035115d7a2a7032f4c19589a9d
Peter <warrenp11@gmail.com>
2021-12-08 18:09:07 -0500

    bubble sort benchmark test

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 const Benchmark = require("benchmark");
-const { linearSearch, binarySearch } = require("./search");
+const { bubbleSort } = require("./sort");
 
 const numbers = [];
-for (let i = 1; i <= 1000000; i++) {
-  // populate the array with the numbers 1 to 1,000,000
-  numbers.push(i);
+for (let i = 0; i < 10000; i++) {
+  numbers.push(Math.floor(Math.random() * 10000) + 1);
 }
-
-// grab the last number in the array as the number we want to find
-// const target = numbers[numbers.length - 1];
-
-// try grabbing first number in the array as the target number
-const target = numbers[0];
 
 const suite = new Benchmark.Suite();
 
 suite
-  .add("linear search", function () {
-    linearSearch(numbers, target);
-  })
-  .add("binary search", function () {
-    binarySearch(numbers, target, 0, numbers.length - 1);
+  .add("bubble sort", function () {
+    const testArray = [...numbers];
+    bubbleSort(testArray);
   })
   .on("complete", function () {
-    // loop over and print each result
     this.forEach((result) =>
       console.log(
         `${result.name} averaged ${result.stats.mean * 1000} milliseconds.`
